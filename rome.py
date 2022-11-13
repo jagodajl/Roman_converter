1d835e5bab69b7847d737f443e35ae55392ed92d
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-11-13 18:32:54 +0100

    Fifth commit

--- rome.py
+++ rome.py
@@ -1,10 +1,17 @@
-ROMAN_NUMERALS = ('I', 'V')
+ROMAN_NUMERALS = ('I', 'V', 'X')
 def add(augend, addend):
     if not isinstance(augend, str) or not isinstance(addend, str):
         raise ValueError
-    simple_sum = augend + addend
-    if any(char != 'I' for char in simple_sum):
+
+    simple_augend = augend.replace('IV', 'IIII')
+    simple_addend = addend.replace('IV', 'IIII')
+
+    simple_sum = simple_augend + simple_addend
+
+    if any(char not in ROMAN_NUMERALS for char in simple_sum):
         raise ValueError
 
-    canonicalised_sum = simple_sum.replace('IIIII', 'V').replace('IIII', 'IV')
+    ordered_sum = ''.join(reversed(sorted(simple_sum)))
+
+    canonicalised_sum = ordered_sum.replace('IIIII', 'V').replace('IIII', 'IV').replace('VV', 'X').replace('VIV', 'IX')
     return canonicalised_sum
